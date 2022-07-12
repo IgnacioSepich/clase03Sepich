4dea6cd8be36ffdbf33fc2b34c9f9706b7899514
ignacio sepich <ignaciosepich@gmail.com>
2022-07-11 22:33:48 -0300

    first commit

--- main.js
+++
@@ -1,9 +0,0 @@
-let entrada = prompt("Ingresa tu nombre");
-entrada = entrada.toUpperCase();
-alert ("Bienvenido " + entrada);
-
-while (entrada != "OK" ){
-alert(entrada + " , por cualquier consulta, no dudes en comunicarte con nosotros");
-entrada = prompt("Ingrese OK para ir al Sitio Web");
-entrada = entrada.toUpperCase();
-}
